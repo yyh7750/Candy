0f86e69067875ec263434b28374ede0926e67f19
YooYounghun <yyh7750@gmail.com>
2023-03-24 13:10:54 +0900

    맥주 상세조회 완료

--- BE/candy/src/main/java/com/project/candy/likes/repository/LikesRepository.java
+++ BE/candy/src/main/java/com/project/candy/likes/repository/LikesRepository.java
@@ -1,11 +1,12 @@
-package com.project.candy.like.repository;
+package com.project.candy.likes.repository;
 
 import com.project.candy.beer.entity.Beer;
-import com.project.candy.like.entity.Like;
+import com.project.candy.likes.entity.Likes;
 import com.project.candy.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+
 import java.util.Optional;
 
 /**
@@ -14,15 +15,16 @@ import java.util.Optional;
  * date           : 2023-03-21
  * description    :
  */
-public interface LikeRepository extends JpaRepository<Like, Long> {
+
+public interface LikesRepository extends JpaRepository<Likes, Long> {
 
 //  @Query(nativeQuery = true, value = "select * from like where like.beer_id = :beerId and like.user_id = :userId")
 //  Optional<Like> readLikeSelectedByUser(Long beerId, long userId);
 
   // 찜 등록 시 이전에 등록했다가 해제한 경우를 고려하여 is_delete가 true든 false든 둘 다 불러오는 메소드
-  Optional<Like> findByUserAndBeer(User user, Beer beer);
+  Optional<Likes> findByUserAndBeer(User user, Beer beer);
 
   // 찜하기 해제하지 않은 데이터만 찾는 메소드
-  @Query(nativeQuery = true, value = "select * from `like` where (`like`.user_id = :user_id) and (`like`.beer_id = :beer_id) and (`like`.is_delete = false)")
-  Like readByUserAndBeerAndIsDeleteFalse(@Param(value = "user_id") long userId, @Param(value = "beer_id") long beerId);
+  @Query(nativeQuery = true, value = "select * from likes where (user_id = :user_id) and (beer_id = :beer_id) and (is_delete = false)")
+  Likes readByUserAndBeerAndIsDeleteFalse(@Param("user_id") long userId, @Param("beer_id") long beerId);
 }
